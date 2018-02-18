e6d874333f1d55e503e3d7d00e6307038541abfe
Christopher Watson <christopher_watson@icloud.com>
2018-02-18 15:39:51 -0500

    vinylNum

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,17 @@
 $("#player-score").text(0);
+var ninaHTML = $("#nina")
+var stevieHTML = $("#stevie")
+var eaglesHTML = $("#eagles")
+var andyHTML = $("#andy")
+
 
 var vinylGame = {
   randNum: 0,
   vinylClick: 0,
   playerScore: 0,
+  vinylNum: 0,
   didWin: false,
+  vinylClear: null,
 
   reset: function(){
   this.randNum = Math.floor(Math.random() * 120) + 19;
@@ -12,7 +19,26 @@ var vinylGame = {
     console.log("randNum: " + this.randNum);
   },
 
+  vinylNum: function(){
+    this.vinylNum = Math.floor(Math.random() * 12) + 1;
+    $("#nina").val(this.vinylNum);
+    this.vinylNum = Math.floor(Math.random() * 12) + 1;
+    $("#stevie").val(this.vinylNum);
+    this.vinylNum = Math.floor(Math.random() * 12) + 1;
+    $("#eagles").val(this.vinylNum);
+    this.vinylNum = Math.floor(Math.random() * 12) + 1;
+    $("#andy").val(this.vinylNum);
+      console.log("Nina: " + ninaHTML.val());
+      console.log("Stevie: " + stevieHTML.val());
+      console.log("Eagles: " + eaglesHTML.val());
+      console.log("Andy: " + andyHTML.val());
+  },
+
   vinylClick: function(){
+
+  },
+
+  vinylClear: function(){
 
   },
 
@@ -20,5 +46,6 @@ var vinylGame = {
 
 $(document).ready(function(){
   vinylGame.reset();
+  vinylGame.vinylNum();
 
 });
